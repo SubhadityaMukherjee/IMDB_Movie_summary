568abf563273b6c5f73cdeab061f51211d2dadd4
Subhaditya <msubhaditya@gmail.com>
2019-03-17 15:52:54 +0530

    req.txt

--- imdb.py
+++ imdb.py
@@ -6,13 +6,12 @@ import re
 def getLinks(name):
     c = 0
     link = b'https://www.imdb.com/find?ref_=nv_sr_fn&q={}&s=all'.decode('ASCII').format('+'.join(name.split()))
-    print(link)
     site = urllib.request.urlopen(link)
     soup = BeautifulSoup(site, 'html.parser')
     list_of_links = []
     for link in soup.find_all('a'):
         hre = link.get('href')
-        if c!=2:
+        if c!=1:
             try:
                 if '/title/' in hre and 'fn_al_' in hre:
                     list_of_links.append('https://www.imdb.com'+hre)
@@ -39,12 +38,19 @@ def summary(text):
 def main():
     dirn = input('Enter dir: ')
     lis = os.listdir(dirn)
+    n = len(lis)
+    p = 0
     for a in lis:
         if 'Store' not in a:
-            f = open(dirn+'/'+a+'/'+a+'.txt','a+')
-            l = getinfo(getLinks(a))
-            for b in l:
-                f.write(summary(b))
-            f.close()
+            try:
+                f = open(dirn+'/'+a+'/'+a+'.txt','w+')
+                l = getinfo(getLinks(a))
+                for b in l:
+                    f.write(summary(b))
+                f.close()
+                print('Done for {}-{}/{}'.format(a,str(p),str(n)))
+            except:
+                pass
+            p+=1
 main()
 
